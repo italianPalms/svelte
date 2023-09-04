3f1970196c6581a183c129d29361b03a0a761951
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-09-04 23:57:03 +0200

    add svelte files and h1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import './page.css';
+    let name = 'Svelte';
+    import Nested from './Nested.svelte';
+    let string = 'this string contains some <strong>HTML!!!</strong>';
+</script>
+
+<h1>Hello {name}!</h1>
+
+<p>This is a paragraph</p>
+<Nested />
+
+<p>{@html string}</p>
+
+
+
+
+
+<style>
+    p{
+        font-size: 2em;
+    }
+   
+</style>
